add5258eec9836e2cd2f24d997d33490829f55f2
Taralas209 <alexej.zikunow@gmail.com>
2023-09-21 16:05:27 +0200

    Add category to Recipes model

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,6 @@ from config import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
