89f6062a9441c626dc6e6e4da1a369590bc0cddf
Taralas209 <alexej.zikunow@gmail.com>
2023-09-23 11:39:50 +0200

    add new model SubscriptionPlan

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import json
 
 
 class User(models.Model):
@@ -52,3 +53,24 @@ class Ingredients(models.Model):
     class Meta:
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
+
+
+class SubscriptionPlan(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='subscription_plans',
+                             verbose_name='Пользователь')
+    start_date = models.DateField(verbose_name='Дата начала')
+    end_date = models.DateField(verbose_name='Дата окончания')
+    daily_plans = models.TextField(verbose_name='Планы на каждый день')
+
+    class Meta:
+        verbose_name = 'План подписки'
+        verbose_name_plural = 'Планы подписки'
+
+    def __str__(self):
+        return f"{self.user} - {self.start_date} - {self.end_date}"
+
+    def set_daily_plans(self, daily_plans_dict):
+        self.daily_plans = json.dumps(daily_plans_dict)
+
+    def get_daily_plans(self):
+        return json.loads(self.daily_plans)
